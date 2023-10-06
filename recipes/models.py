1bc2a3439716fab64d67f4b865ed6021ab777e69
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-10-06 07:17:35 +0000

    Add recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from djrichtextfield.models import RichTextField
 
-# Create your models here.
+
+# Code taken from "theRecipeCollective"
+# bySandraBergstrom with some modifications.
+
+class LikedManager(models.Manager):
+    def get_queryset(self, user):
+        return super().get_queryset().filter(likes=user)
+
+# Status for user to set if recipe should be draft or pubilished.
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+"""
+A recipe model that enables users to create, view, update, and delete recipes.
+"""
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    baking_time = models.PositiveIntegerField(blank=False)
+    ingredients = RichTextField(max_length=10000, null=False, blank=False)
+    instructions = RichTextField(max_length=10000, null=False, blank=False)
+    image = CloudinaryField('image', default='placeholder', null=False)
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+    posted_date = models.DateField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        User, related_name='like', default=None, blank=True)
+    liked_recipes = LikedManager
+
+    class Meta:
+        ordering = ['-posted_date']
+
+    def __str__(self):
+        return self.title
