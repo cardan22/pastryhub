e76ed58021728de195b7e1b134b3e1eb2bf2dca2
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-10-06 10:05:10 +0000

    Set up view, templates and connect urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+class RecipesList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by("-posted_date")
+    template_name = "index.html"
+    paginate_by = 6
