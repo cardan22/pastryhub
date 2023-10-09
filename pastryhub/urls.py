0f4e2e6c47836c01a145181328c3281955a87ae3
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-10-09 10:27:35 +0000

    Fix: Correct spelling mistake for 'recipe'

--- pastryhub/urls.py
+++ pastryhub/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("recipes.urls"), name="recipes-urls"),
+    path("", include('recipes.urls'), name='recipes_urls'),
     path('summernote/', include('django_summernote.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Recipe
 
-class RecipesList(generic.ListView):
+
+class RecipeList(generic.ListView):
+    """ View all recipes"""
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by("-posted_date")
     template_name = "index.html"
