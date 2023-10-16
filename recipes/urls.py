0f7da4c541c71db8c3d245f2311e4f9ff43814ba
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-10-16 10:20:22 +0000

    Implement recipe update view

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.RecipeList.as_view(), name="home"),
     path("my_recipes/", views.MyRecipesList.as_view(), name="my_recipes"),
     path("add_recipe/", views.AddRecipe.as_view(), name="add_recipe"),
+    path(
+        "update_recipe/<int:pk>/", views.UpdateRecipe.as_view(), name="update"
+        ),
     path("<slug:pk>/", views.RecipeDetail.as_view(), name="recipe_detail"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, UpdateView, ListView, DetailView
 from .models import Recipe, FavoriteRecipe
 from .forms import RecipeForm
 
 
 class RecipeList(ListView):
-    """View all recipes"""
+    """
+    View to display a list of all published recipes.
+
+    This view retrieves all recipes with 'status' set to 1
+    (published) and orders them by their posting date.
+    """
 
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by("-posted_date")
@@ -15,7 +20,11 @@ class RecipeList(ListView):
 
 
 class RecipeDetail(DetailView):
-    """View to display details of a recipe."""
+    """
+    View to display the details of a recipe.
+
+    This view retrieves the details of a single recipe using its primary key.
+    """
 
     model = Recipe
     template_name = "recipe_detail.html"
@@ -23,7 +32,11 @@ class RecipeDetail(DetailView):
 
 
 class AddRecipe(LoginRequiredMixin, CreateView):
-    """Add recipe view"""
+    """
+    View to add a new recipe.
+
+    This view allows authenticated users to create and submit a new recipe.
+    """
 
     template_name = "add_recipe.html"
     model = Recipe
@@ -35,8 +48,30 @@ class AddRecipe(LoginRequiredMixin, CreateView):
         return super(AddRecipe, self).form_valid(form)
 
 
+class UpdateRecipe(LoginRequiredMixin, UpdateView):
+    """
+    View to update an existing recipe.
+
+    This view allows authenticated users to update an already submitted recipe.
+    """
+
+    model = Recipe
+    form_class = RecipeForm
+    template_name = "update_recipe.html"
+    success_url = "/my_recipes/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(UpdateView, self).form_valid(form)
+
+
 class MyRecipesList(LoginRequiredMixin, ListView):
-    """ My Recipes view """
+    """
+    View to display a list of recipes created by the logged-in user.
+
+    This view retrieves and displays all recipes
+    created by the currently logged-in user.
+    """
 
     model = Recipe
     queryset = Recipe.objects.order_by("-posted_date")
