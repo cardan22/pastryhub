fc4e1d68252ffc7a8f081fb2fd226e001b90f959
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-10-31 15:19:00 +0000

    Remove trailing slashes from urls.py and adjust index.html and CSS

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 
 urlpatterns = [
     path("", views.RecipeList.as_view(), name="home"),
-    path("my-recipes/", views.MyRecipesList.as_view(), name="my_recipes"),
-    path("add-recipe/", views.AddRecipe.as_view(), name="add_recipe"),
+    path("my-recipes", views.MyRecipesList.as_view(), name="my_recipes"),
+    path("add-recipe", views.AddRecipe.as_view(), name="add_recipe"),
     path(
         "update-recipe/<int:pk>", views.UpdateRecipe.as_view(), name="update"
         ),
@@ -14,7 +14,7 @@ urlpatterns = [
         "add-favorit-recipe/<int:id>", views.AddFavoriteRecipe.as_view(),
         name="add_favorite_recipe"),
     path(
-        'favorite-recipes/', views.FavoriteRecipesList.as_view(),
+        "favorite-recipes", views.FavoriteRecipesList.as_view(),
         name='favorite_recipes'),
     path("<slug:pk>", views.RecipeDetail.as_view(), name="recipe_detail"),
 ]
